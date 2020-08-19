88ecb47aa4922c3944820e0789418a9f6b250953
sojjyCodes <ebenezeralagbe26@gmail.com>
2020-08-19 12:45:37 +0100

    Fixed a little bug

--- src/components/signup.component.js
+++ src/components/signup.component.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 export default class SignUp extends Component {
     render() {
@@ -8,27 +8,45 @@ export default class SignUp extends Component {
 
                 <div className="form-group">
                     <label>First name</label>
-                    <input type="text" className="form-control" placeholder="First name" />
+                    <input
+                        type="text"
+                        className="form-control"
+                        placeholder="First name"
+                    />
                 </div>
 
                 <div className="form-group">
                     <label>Last name</label>
-                    <input type="text" className="form-control" placeholder="Last name" />
+                    <input
+                        type="text"
+                        className="form-control"
+                        placeholder="Last name"
+                    />
                 </div>
 
                 <div className="form-group">
                     <label>Email address</label>
-                    <input type="email" className="form-control" placeholder="Enter email" />
+                    <input
+                        type="email"
+                        className="form-control"
+                        placeholder="Enter email"
+                    />
                 </div>
 
                 <div className="form-group">
                     <label>Password</label>
-                    <input type="password" className="form-control" placeholder="Enter password" />
+                    <input
+                        type="password"
+                        className="form-control"
+                        placeholder="Enter password"
+                    />
                 </div>
 
-                <button type="submit" className="btn btn-primary btn-block">Sign Up</button>
+                <button type="submit" className="btn btn-primary btn-block">
+                    Sign Up
+                </button>
                 <p className="forgot-password text-right">
-                    Already registered <a href="#">sign in?</a>
+                    Already registered <a href="/">sign in?</a>
                 </p>
             </form>
         );
