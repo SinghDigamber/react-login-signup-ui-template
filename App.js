747cb2c3bce5c4a0808f6834858aa21790fb86c3
KillytheBid <70246094+KillytheBid@users.noreply.github.com>
2022-04-20 16:47:33 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import Login from "./components/login.component";
+import SignUp from "./components/signup.component";
+import ui_test from "./components/ui_test.component";
+
+
+function App() {
+  return (<Router>
+    <div className="App">
+      <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+        <div className="container">
+          <Link className="navbar-brand" to={"/sign-in"}  >Down To Food-Truck </Link>
+          <a href={"/sign-in"}><img src="https://image.shutterstock.com/image-vector/truck-icon-vector-illustration-on-260nw-390772729.jpg" alt="W3Schools.com" width="70px" height="70px"></img></a>
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link className="nav-link" to={"/sign-in"}>Login</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to={"/sign-up"}>Sign up</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to={"/ui_test"}>UI</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      <div className="auth-wrapper">
+        <div className="auth-inner">
+          <Switch>
+            <Route exact path='/' component={Login} />
+            <Route path="/sign-in" component={Login} />
+            <Route path="/sign-up" component={SignUp} />
+            <Route path="/ui_test" component={ui_test} />
+          </Switch>
+        </div>
+      </div>
+    </div></Router>
+  );
+}
+
+export default App;
